bbe1e36752feb2c49f17c9f113c3cee96b08eff1
Robert Bethencourt <robert.bethencourt@gmail.com>
2018-07-17 18:36:30 -0400

    navigate without transitions

--- src/Pages/Page3.elm
+++ src/Pages/Page3.elm
@@ -1,8 +1,28 @@
 module Pages.Page3 exposing (..)
 
 import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
 
 
 view : Html msg
 view =
-    div [] [ text "Page 3" ]
+    post
+
+
+post : Html msg
+post =
+    div [ class "post" ]
+        [ div [ class "post-image" ] []
+        , div [ class "post-heading" ] []
+        , div [ class "post-content" ]
+            (List.map postContentLine (List.range 1 5))
+        , div [ class "post-content" ]
+            (List.map postContentLine (List.range 1 2))
+        , div [ class "post-content" ]
+            (List.map postContentLine (List.range 1 7))
+        ]
+
+
+postContentLine : Int -> Html msg
+postContentLine _ =
+    div [ class "post-content-line" ] []
